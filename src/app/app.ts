15cc87df85fa6c2cef87dd63c76ea653207c5b06
RicardoFrancoDelgado <rf041555@gmail.com>
2026-02-28 14:20:36 -0300

    fix: subindo app da aplicação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
-import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { PrimaryButton } from './_components/primary-button/primary-button';
+import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificado } from './_components/item-certificado/item-certificado';
+import { BaseUi } from './_components/base-ui/base-ui';
+import { Certificados } from './pages/certificados/certificados';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, PrimaryButton],
+  imports: [Navbar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
